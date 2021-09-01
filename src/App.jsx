76c61f32fb3199d7369bd99ece0b515b20429456
ToufiqurRahmanTamkin <tamkin22rahman@gmail.com>
2021-09-01 16:48:20 +0600

    setup project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import {ChannelContainer, ChannelListContainer} from './components';
+import './App.css';
+
+const cookies = new Cookies();
+const apiKey = 'qgtk9ttyha7j';
+const authToken = cookies.get("token");
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
     return (
-        <div>
-            hello world
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
         </div>
     )
 }
