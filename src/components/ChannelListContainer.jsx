55d08a244bd8094bef52aec1b2d7200e6c68ca40
ToufiqurRahmanTamkin <tamkin22rahman@gmail.com>
2021-09-05 00:17:00 +0600

    added auth in client

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
 import Cookies from "universal-cookie";
+
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 import HospitalIcon from "../assets/hospital.png";
 import LogoutIcon from "../assets/logout.png";
